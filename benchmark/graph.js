c0631ee5964ed951af06760b694e9b3c1fb71bcd
Ryan Rowland <ryan.w.rowland@gmail.com>
2016-04-04 18:53:40 -0700

    Removed comments from quiche demo code

--- benchmark/graph.js
+++ benchmark/graph.js
@@ -1,6 +1,5 @@
 var Quiche = require('quiche');
 
-
 function Graph() { };
 
 Graph.getUrl = function(labels, dataSet1, dataSet2) {
@@ -8,13 +7,13 @@ Graph.getUrl = function(labels, dataSet1, dataSet2) {
   bar.setWidth(600);
   bar.setHeight(400);
   bar.setTitle('Operations per second (thousands)');
-  bar.setBarSpacing(4); // 6 pixles between bars/groups
-  bar.setLegendBottom(); // Put legend at bottom
+  bar.setBarSpacing(4);
+  bar.setLegendBottom();
 
   bar.addData(dataSet1.data, dataSet1.name, 'FF0000');
   bar.addData(dataSet2.data, dataSet2.name, '0000FF');
 
-  bar.setAutoScaling(); // Auto scale y axis
+  bar.setAutoScaling();
   bar.addAxisLabels('x', labels);
 
   return bar.getUrl(true).replace(/%2B/g, '+');
